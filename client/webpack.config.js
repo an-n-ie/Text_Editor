75a2e444f112dc0a79a9ac5f8d644192325f50d5
Annie Hoang <anniehoang.anh@gmail.com>
2022-10-20 16:02:55 -0400

    WebpackPwaManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template:'./index.html',
-        title:'Contact Cards'
+        title:'J.A.T.E'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -29,9 +29,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Another text editor for you',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
